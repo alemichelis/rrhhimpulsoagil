40504fab7b8ba7e9042d430076f2dc47bf755934
Impulso Agil <alemichelis84@gmail.com>
2026-06-24 14:13:29 -0300

    config deploy Railway

--- main.py
+++ main.py
@@ -551,4 +551,6 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
